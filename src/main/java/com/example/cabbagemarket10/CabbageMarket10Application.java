a3909964ee6fd55baa045a18e29513e136cec1a0
pcb2002 <machat350@gmail.com>
2026-06-23 16:50:39 +0900

    [feat]: init spring boot project

---
+++ src/main/java/com/example/cabbagemarket10/CabbageMarket10Application.java
@@ -0,0 +1,13 @@
+package com.example.cabbagemarket10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabbageMarket10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabbageMarket10Application.class, args);
+    }
+
+}
