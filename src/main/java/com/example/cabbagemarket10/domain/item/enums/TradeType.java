f4bd957ca90a4e81cf7bb5fcabc1ae899a4dec34
pcb2002 <machat350@gmail.com>
2026-06-25 17:18:04 +0900

    [feat]: 택배 제외

--- src/main/java/com/example/cabbagemarket10/domain/item/enums/TradeType.java
+++ src/main/java/com/example/cabbagemarket10/domain/item/enums/TradeType.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TradeType {
     DIRECT("직거래"),
-    DELIVERY("택배거래"),
     AUCTION("경매");
 
     private final String description;
