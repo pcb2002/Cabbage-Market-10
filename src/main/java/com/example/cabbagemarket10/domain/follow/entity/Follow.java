64f1d2f5b639c4dc9e073d0db1621f04d56a7712
silverThunder09 <jeg9017@gmail.com>
2026-06-29 18:55:49 +0900

    feat: 팔로우 제약조건 추가

--- src/main/java/com/example/cabbagemarket10/domain/follow/entity/Follow.java
+++ src/main/java/com/example/cabbagemarket10/domain/follow/entity/Follow.java
@@ -1,6 +1,7 @@
 package com.example.cabbagemarket10.domain.follow.entity;
 
 import com.example.cabbagemarket10.domain.client.entity.Client;
+import jakarta.persistence.CheckConstraint;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -21,7 +22,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "follow")
+@Table(
+        name = "follow",
+        check = @CheckConstraint(constraint = "follower_id <> following_id")
+)
 @IdClass(FollowId.class)
 @EntityListeners(AuditingEntityListener.class)
 public class Follow {
