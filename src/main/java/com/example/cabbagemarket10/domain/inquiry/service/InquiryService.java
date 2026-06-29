56563e8f412fbd903d1d288b2dca28f24ed4518d
hanbyeol.yang <beuri97@gmail.com>
2026-06-29 11:17:39 +0900

    fix: add missing/losted codes

--- src/main/java/com/example/cabbagemarket10/domain/inquiry/service/InquiryService.java
+++ src/main/java/com/example/cabbagemarket10/domain/inquiry/service/InquiryService.java
@@ -61,10 +61,16 @@ public class InquiryService {
 
     @Transactional
     public void deleteInquiry(Long inquiryId, Long authorId) {
-      inquiryLogRepository.delete(inquiryLog);
-  
-      return InquiryUpdateResponse.from(inquiryLog);
-  }
+
+        InquiryLog log = inquiryLogRepository.findRootInquiryByIdWithAuthor(inquiryId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.INQUIRY_NOT_FOUND));
+
+        if (!log.getAuthor().getId().equals(authorId)) {
+            throw new BusinessException(ErrorCode.FORBIDDEN);
+        }
+
+        inquiryLogRepository.delete(log);
+    }
 
     @Transactional
     public InquiryUpdateResponse updateInquiry(Long inquiryId, Long authorId, InquiryUpdateRequest request) {
