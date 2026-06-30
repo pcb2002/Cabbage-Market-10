77918d13a6c243d3bfb3cdcc99059686ac644898
hanbyeol.yang <beuri97@gmail.com>
2026-07-01 04:56:06 +0900

    fix: fix validation annotation

--- src/main/java/com/example/cabbagemarket10/domain/chat/dto/websocket/ChatMessageDto.java
+++ src/main/java/com/example/cabbagemarket10/domain/chat/dto/websocket/ChatMessageDto.java
@@ -2,11 +2,12 @@ package com.example.cabbagemarket10.domain.chat.dto.websocket;
 
 import com.example.cabbagemarket10.domain.chat.entity.MessageType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ChatMessageDto(
         @NotBlank
         String content,
-        @NotBlank
+        @NotNull
         MessageType contentType
 ) {
 }
