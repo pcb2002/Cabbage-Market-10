762bc32189b121779f00a49313eea96e31ca40e0
hanbyeol.yang <beuri97@gmail.com>
2026-06-30 19:08:35 +0900

    remove roomId property
    - will not pass it through body

--- src/main/java/com/example/cabbagemarket10/domain/chat/dto/websocket/ChatMessageDto.java
+++ src/main/java/com/example/cabbagemarket10/domain/chat/dto/websocket/ChatMessageDto.java
@@ -3,7 +3,6 @@ package com.example.cabbagemarket10.domain.chat.dto.websocket;
 import jakarta.validation.constraints.NotBlank;
 
 public record ChatMessageDto(
-        long roomId,
         @NotBlank
         String content
 ) {
