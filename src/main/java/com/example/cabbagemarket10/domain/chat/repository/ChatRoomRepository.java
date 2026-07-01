ec66de9dab6b33e18f6ff7f20eb4464cf8eca741
hanbyeol.yang <beuri97@gmail.com>
2026-07-01 21:51:49 +0900

    fix: 채팅방 목록 조회 쿼리 수정

--- src/main/java/com/example/cabbagemarket10/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/example/cabbagemarket10/domain/chat/repository/ChatRoomRepository.java
@@ -11,12 +11,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
 
-    @Query("""
+    @Query(value = """
             SELECT new com.example.cabbagemarket10.domain.chat.dto.restful.ChatRoomDetail
                         (cr.id, i.title, cr.lastMessageAt)
             FROM ChatRoom cr
-            join fetch cr.item i
-            join fetch cr.createdBy cbci
-            WHERE cbci.id = :clientId OR i.seller.id = :clientId""")
+            join cr.item i
+            join cr.createdBy cbci
+            WHERE cbci.id = :clientId OR i.seller.id = :clientId""",
+            countQuery = """
+                    SELECT count(cr)
+                    FROM ChatRoom cr
+                    join cr.item i
+                    join cr.createdBy cbci
+                    WHERE cbci.id = :clientId OR i.seller.id = :clientId""")
     Page<ChatRoomDetail> findByClientId(Long clientId, Pageable pageable);
 }
