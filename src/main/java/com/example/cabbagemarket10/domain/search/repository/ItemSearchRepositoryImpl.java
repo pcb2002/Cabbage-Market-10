421fe94f68021604c2369be9fc4cf6a2ff1620de
silverThunder09 <jeg9017@gmail.com>
2026-07-01 09:29:35 +0900

    fix: 중복되는 조건 is_deleted 제거

--- src/main/java/com/example/cabbagemarket10/domain/search/repository/ItemSearchRepositoryImpl.java
+++ src/main/java/com/example/cabbagemarket10/domain/search/repository/ItemSearchRepositoryImpl.java
@@ -44,7 +44,6 @@ public class ItemSearchRepositoryImpl implements ItemSearchRepository {
 
         BooleanBuilder where = new BooleanBuilder();
         where.and(item.isDraft.isFalse());
-        where.and(item.isDeleted.isFalse());
 
         if (categoryId != null) {
             where.and(item.category.id.eq(categoryId));
