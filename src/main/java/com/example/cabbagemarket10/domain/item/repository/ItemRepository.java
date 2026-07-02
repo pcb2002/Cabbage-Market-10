31b88699b800de8a429544a17881f5ed7323e2c1
silverThunder09 <jeg9017@gmail.com>
2026-07-02 12:28:46 +0900

    fix: 충돌 해결

--- src/main/java/com/example/cabbagemarket10/domain/item/repository/ItemRepository.java
+++ src/main/java/com/example/cabbagemarket10/domain/item/repository/ItemRepository.java
@@ -35,13 +35,6 @@ public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositor
     Optional<Item> findById(Long id);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("""
-            select i
-            from Item i
-            where i.id = :itemId
-              and i.isDeleted = false
-              and i.isDraft = false
-            """)
     @Query("select i from Item i where i.id = :itemId and i.isDraft = false")
     Optional<Item> findByIdForUpdate(@Param("itemId") Long itemId);
 
